b1207c8cd30bcabfc949794d193c7a82bc0e2fdf
xubing <xubing0129@163.com>
2019-12-31 18:05:42 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+.size{
+  width: 100%;
+  height: 100%;
+}
+html,body{
+  .size;
+  overflow: hidden;
+  margin: 0;
+  padding: 0 20%;
+  background-image:linear-gradient(#01528b,#001b2d);
+}
+#app {
+  .size;
+   .ivu-layout{
+     background: none;
+   }
+}
+</style>
---
+++ src/view/single-page/home/home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+    <Row :gutter="20">
+      <div> 1</div>
+    </Row>
+    <Row :gutter="20" style="margin-top: 10px;">
+           22
+    </Row>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    //
+  }
+}
+</script>
+
+<style lang="less">
+  .ivu-row{
+    background: #fff;
+  }
+.count-style{
+  font-size: 50px;
+}
+</style>
